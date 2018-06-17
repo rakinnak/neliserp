9972e6dd1f385fd756fdd319997ba3af48b11bf6
Rakinnak.S <rakinnak.s@gmail.com>
2018-06-17 15:19:54 +0700

    Correct folder name in add-font.php

--- docs/add-font.php
+++ docs/add-font.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $fontname = TCPDF_FONTS::addTTFfont('fonts/THSarabunNew.ttf', 'TrueTypeUnicode', '');
 
